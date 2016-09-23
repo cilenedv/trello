35ea857f9d32d3b75452474aeea6cf662367b2a4
cilenedv <cilenedelacruz@gmail.com>
2016-09-23 03:35:14 -0500

    subiendo cambios en css y js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,30 @@
-window.addEventListener("load", function() {
- 	document.getElementById("boton").style.display="none";
- 	document.getElementById("icon").style.display="none";
- 	var caja = document.getElementById("caja");
- 	var entrada=document.getElementById("texto");
- 	document.getElementById("boton");
- 	document.getElementById("icon");
- 	document.getElementById("nombre");
- 	entrada.addEventListener("click",function(){
- 		texto.style.display="none";
- 		agregar();
- 		boton.style.display="block";
- 		icon.style.display="block";
- 		document.getElementById("caja").classList.add("fondo-form");
-
- 	});
- 	function agregar() {
-        var mensaje = document.createElement("input");
-        mensaje.setAttribute("id","nombre");
-       	mensaje.placeholder = "Añadir una lista...";
-       	mensaje.classList.add("texto2");
-        caja.insertBefore(mensaje, caja.childNodes[0]);
-    }
-    boton.addEventListener("click",imprimir);
-    function imprimir(){
-    	boton.style.display="none";
-    	icon.style.display="none";
-    	nombre.style.display="none";
-    	valor = document.getElementById("nombre").value;
-    	var contenedor=document.createElement("div");
-		contenedor.innerHTML = valor;
-		caja.insertBefore(contenedor,caja.childNodes[0]);
-		var crear=document.createElement("a");
-		caja.insertBefore(crear, caja.childNodes[1]);
-		var t = document.createTextNode("Añadir una tarjeta"); 
-		crear.appendChild(t);
-		crear.classList.add("link")
-
-
-    }
-
+window.addEventListener("load",function(){
+    var texto1 = document.getElementById('texto1');
+    var mensaje = document.getElementById('mensaje');
+    mensaje.style.display = "none";
+    var btnForm = document.getElementById("btnForm");
+    var inpForm = document.getElementById("inpForm");
+    texto1.addEventListener("click",function(e){
+		e.preventDefault();
+        mensaje.style.display = "block";
+        texto1.style.display = "none";
+        mensaje.style.display="inline-block";
+	});
+    btnForm.addEventListener("click", crear);
+    function crear(){
+        var conten = document.createElement("div");
+        var titulo = document.createElement("h5");
+        var enlace = document.createElement("a");
+        enlace.setAttribute("id","enlace");
+        var nodeTitulo = document.createTextNode(inpForm.value);
+        var nodeNuevo = document.createTextNode("Añadir una tarjeta");
+        titulo.appendChild(nodeTitulo);
+        enlace.appendChild(nodeNuevo);
+        conten.appendChild(titulo);
+        conten.appendChild(enlace);
+        conten.classList.add("tarjeta"); texto1.parentNode.insertBefore(conten,texto1.previousSibling);
+        conten.style.cssFloat = "left";
+        texto1.style.display = "inline-block";
+        mensaje.style.display = "none";
+    }    
 });
-
